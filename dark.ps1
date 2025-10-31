da6624d14f1036e2b05c7829a9af5c0b465654b1
rl <163364897+rileylorgelly@users.noreply.github.com>
2025-10-31 14:53:13 +1300

    refresh explorer to reset theme

--- dark.ps1
+++ dark.ps1
@@ -1,3 +1,6 @@
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "AppsUseLightTheme" -Value 0
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "SystemUsesLightTheme" -Value 0
 Write-Host -f Yellow "done`n"
+
+Stop-Process -Name explorer -Force # refresh explorer (to refresh theme)
+Start-Process -FilePath explorer.exe # just incase
