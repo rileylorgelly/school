212909dbd39328e1372ac397a9c4058280277927
rl <163364897+rileylorgelly@users.noreply.github.com>
2025-10-31 14:32:43 +1300

    set dark added

---
+++ dark.ps1
@@ -0,0 +1,3 @@
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "AppsUseLightTheme" -Value 0
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "SystemUsesLightTheme" -Value 0
+Write-Host -f Yellow "done`n"
